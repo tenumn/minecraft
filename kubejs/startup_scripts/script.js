1e54b69dc2a0d828ef4b15cd0db34faca688dbb4
Tenumn <tenumn@outlook.com>
2022-08-16 12:58:47 +0800

    增加钱币，魔法

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -1,13 +1,9 @@
-// priority: 0
-
-console.info('Hello, World! (You will only see this line once in console, during startup)')
-
 onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-})
+	event.create('gold_coin').displayName('金币');
+	event.create('silver_coin').displayName('银币');
+	event.create('copper_coin').displayName('铜币');
+});
 
 onEvent('block.registry', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+	event.create('livingrock_casing').material('wood').hardness(1.0).displayName('活石机壳');
+});
